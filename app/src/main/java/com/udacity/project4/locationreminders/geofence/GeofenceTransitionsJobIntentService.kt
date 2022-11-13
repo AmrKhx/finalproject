fabf3c09dbf2091a53c20dac42e244a3643a2729
AmrKhx <102529176+AmrKhx@users.noreply.github.com>
2022-11-13 16:16:23 +0200

    GeoForce

--- app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
@@ -2,8 +2,11 @@ package com.udacity.project4.locationreminders.geofence
 
 import android.content.Context
 import android.content.Intent
+import android.text.TextUtils
+import android.util.Log
 import androidx.core.app.JobIntentService
 import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingEvent
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
@@ -11,19 +14,23 @@ import com.udacity.project4.locationreminders.data.local.RemindersLocalRepositor
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.utils.sendNotification
 import kotlinx.coroutines.*
+import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
 import kotlin.coroutines.CoroutineContext
 
 class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
 
     private var coroutineJob: Job = Job()
+    private lateinit var DB: ReminderDataSource
+
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.IO + coroutineJob
+
 
     companion object {
         private const val JOB_ID = 573
 
-        //        TODO: call this to start the JobIntentService to handle the geofencing transition events
+        //         call this to start the JobIntentService to handle the geofencing transition events //done
         fun enqueueWork(context: Context, intent: Intent) {
             enqueueWork(
                 context,
@@ -34,14 +41,32 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
     }
 
     override fun onHandleWork(intent: Intent) {
-        //TODO: handle the geofencing transition events and
-        // send a notification to the user when he enters the geofence area
-        //TODO call @sendNotification
+        val geofenceEvent= GeofencingEvent.fromIntent(intent)
+        val geofenceList:List<Geofence> =geofenceEvent.triggeringGeofences
+
+
+        sendNotification(geofenceList)
+
+
     }
 
-    //TODO: get the request id of the current geofence
     private fun sendNotification(triggeringGeofences: List<Geofence>) {
-        val requestId = ""
+        val requestId = when {
+            triggeringGeofences.isNotEmpty() -> {
+                Log.d("GeofenceService", "Notify" + triggeringGeofences[0].requestId)
+                triggeringGeofences[0].requestId
+            }
+            else -> {
+                Log.e("GeofenceService", "No Trigger Found")
+
+            return
+        }
+    }
+        if(TextUtils.isEmpty(requestId)) return
+        DB = get()
+
+
+
 
         //Get the local repository instance
         val remindersLocalRepository: ReminderDataSource by inject()
